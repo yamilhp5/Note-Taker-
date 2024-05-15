184eddb00cced009f4255a390eaa08e5bb32efeb
yamilhp5 <yamil.hernandezp@gmail.com>
2024-05-15 19:40:28 -0400

    ReadMe updat

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Note-Taker-
+# Note-Taker-Express
+
+# Description 
+The purpose for this assignment was to create a note taking application using Node, Express.js and Render (as per instructor request) that will allow the user 
+to write, save and delete notes. 
+
+# Results 
+![alt text](image-1.png)
+![alt text](image.png)
+![alt text](image-2.png)
+
+
+
+# Usage 
+1. The user downloads content from the repository. 
+2. User will click "Get started" in order to open the note taker application and proceed to enter their notes in the "title" and "text" area. 
+3. "Click" on the save icon on the top right corner to save the note and "+" icon to add another note.
+4. To delete, "Click" the trash icon.
+
+The video walkthrough is here: 
+Repository link: https://github.com/yamilhp5/Note-Taker- 
+
+# Credits 
+All contributions are from this user, https://github.com/yamilhp5. Help from my tutor following material covered in our bootcamp. 
+
+# License 
+MIT License 
+
+# Features 
+Node.js, Express.js, Render 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,22 +28,31 @@ router.post('/notes', (req, res) => {
 
 //  -----  /api/notes/77
 
-router.delete("/notes/:id", (req, res)=>{
-   fs.readFile("./db/db.json", (err, results)=>{
-        if( err){
-            throw err
-        }
+// router.delete("/notes/:id", (req, res)=> {
+//    fs.readFile("./db/db.json", (err, results)=>{
+//         if( err){
+//             throw err
+//         }
 
-        let notelist = JSON.parse(results)
+//         let notelist = JSON.parse(results)
 
-        console.log(notelist)
+//         console.log(notelist)
         
 
        
 
-        //req.params.id
+//         //req.params.id
 
-   })
-});
+//    })
+// });
+router.delete('/notes/:id', (req, res)=> {
+    let data = fs.readFileSync('./db/db.json','utf-8');
+    const dataJSON =JSON.parse(data);
+    const newNotes = dataJSON.filter((note) => {
+        return note.id !== req.params.id;
+    });
+    fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
+    res.json('Note deleted.');
+}); 
 
 module.exports = router; 
